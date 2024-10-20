c2e01288122b6ff0dfe7ebcff6ca9d95f2ff1daa
triptibhatnagar <bhatnagartripti2727@gmail.com>
2024-10-20 11:53:51 +0530

    changes in js

--- Login form/script.js
+++ Login form/script.js
@@ -1,14 +1,9 @@
 document.getElementById('loginForm').addEventListener('submit', function(e){
-    //Prevent the form from submitting
     e.preventDefault();
 
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
-
-    //Check for empty fields and alert the user
-    if(username == "" || password == "") {
-        alert("Fill out both fields")
-    }
+    
     if(username != "" && password != "") {
         alert("Login successfully!")
     }
