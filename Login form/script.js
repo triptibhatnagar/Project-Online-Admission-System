2832ba878e1a05ef182a733104efbcde49b537ad
triptibhatnagar <bhatnagartripti2727@gmail.com>
2024-10-20 12:18:16 +0530

    changes in js file

--- Login form/script.js
+++ Login form/script.js
@@ -1,3 +1,11 @@
+function checkSignup() {
+    const isSignedUp = localStorage.getItem('isSignedUp') == 'true';
+    if(!isSignedUp) {
+        alert("Please sign up before attempting to login");
+        window.location.href = '../Signup form/index.html';
+    }
+}
+window.onload = checkSignup;
 document.getElementById('loginForm').addEventListener('submit', function(e){
     e.preventDefault();
 
--- Signup form/script.js
+++
@@ -1,8 +0,0 @@
-function checkSignup() {
-    const isSignedUp = localStorage.getItem('isSignedUp') == 'true';
-    if(!isSignedUp) {
-        alert("Please sign up before attempting to login");
-        window.location.href = '../Login form/index.html';
-    }
-}
-window.onload = checkSignup;
